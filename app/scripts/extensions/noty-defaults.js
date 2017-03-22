8725b7d773484e8bb53e24e4981d76830cd78e3d
mac <luckysevenbear@gmail.com>
2017-03-22 18:31:02 +0200

    added extension module

---
+++ app/scripts/extensions/noty-defaults.js
@@ -0,0 +1,119 @@
+  $.noty.layouts.top = {
+      name     : 'top',
+      options  : {},
+      container: {
+          object  : '<ul class="bzrdash-noty-top" />',
+          selector: 'ul.bzrdash-noty-top',
+          style   : function() {}
+      },
+      parent   : {
+          object  : '<li />',
+          selector: 'li',
+          css     : {}
+      },
+      css      : {
+          display: 'none'
+      },
+      addClass : ''
+  };
+
+  $.noty.layouts.topLeft = {
+      name     : 'topLeft',
+      options  : {},
+      container: {
+          object  : '<ul class="bzrdash-noty-top-left" />',
+          selector: 'ul.bzrdash-noty-top-left',
+          style   : function() {}
+      },
+      parent   : {
+          object  : '<li />',
+          selector: 'li',
+          css     : {}
+      },
+      css      : {
+          display: 'none',
+          width  : '300px'
+      },
+      addClass : ''
+  };
+
+  $.noty.layouts.topRight = {
+      name     : 'topRight',
+      options  : {
+      },
+      container: {
+          object  : '<ul class="bzrdash-noty-top-right" />',
+          selector: 'ul.bzrdash-noty-top-right',
+          style   : function() {
+          }
+      },
+      parent   : {
+          object  : '<li />',
+          selector: 'li',
+          css     : {}
+      },
+      css      : {
+          display: 'none',
+          width  : '300px'
+      },
+      addClass : ''
+  };
+//
+  $.noty.layouts.bottom = {
+      name     : 'bottom',
+      options  : {},
+      container: {
+          object  : '<ul class="bzrdash-noty-bottom" />',
+          selector: 'ul.bzrdash-noty-bottom',
+          style   : function() {}
+      },
+      parent   : {
+          object  : '<li />',
+          selector: 'li',
+          css     : {}
+      },
+      css      : {
+          display: 'none'
+      },
+      addClass : ''
+  };
+
+  $.noty.layouts.bottomLeft = {
+      name     : 'bottomLeft',
+      options  : {},
+      container: {
+          object  : '<ul class="bzrdash-noty-bottom-left" />',
+          selector: 'ul.bzrdash-noty-bottom-left',
+          style   : function() {}
+      },
+      parent   : {
+          object  : '<li />',
+          selector: 'li',
+          css     : {}
+      },
+      css      : {
+          display: 'none',
+          width  : '300px'
+      },
+      addClass : ''
+  };
+
+  $.noty.layouts.bottomRight = {
+      name     : 'bottomRight',
+      options  : {},
+      container: {
+          object  : '<ul class="bzrdash-noty-bottom-right" />',
+          selector: 'ul.bzrdash-noty-bottom-right',
+          style   : function() {}
+      },
+      parent   : {
+          object  : '<li />',
+          selector: 'li',
+          css     : {}
+      },
+      css      : {
+          display: 'none',
+          width  : '300px'
+      },
+      addClass : ''
+  };
